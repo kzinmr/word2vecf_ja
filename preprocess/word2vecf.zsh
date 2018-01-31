23dff3fee3728b7885f1e93a0f3d9ec166ebe057
Kazuki Inamura <kzinmr109@gmail.com>
2018-01-31 22:04:58 +0900

    add some pars

--- preprocess/word2vecf.zsh
+++ preprocess/word2vecf.zsh
@@ -1,9 +1,13 @@
 #!/usr/bin/zsh
 
-deps=${1} # /data/user/wikipedia_dep/wikipedia.deps.gz
+deps=${1} # /data/user/wikipedia_dep/wikipedia.deps
+p=${2-12}
+mincount=100
+dim=256
+ns=15
 dir=$deps:h
 cv=${dir}/cv
 wv=${dir}/wv
-out=${dir}/dim256vecs
-../word2vecf/count_and_filter -train $deps -cvocab $cv -wvocab $wv -min-count 100
-../word2vecf/word2vecf -train $deps -wvocab $wv -cvocab $cv -output $out -size 256 -negative 15 -threads 12
+out=${dir}/dim${dim}vecs
+../word2vecf/count_and_filter -train $deps -cvocab $cv -wvocab $wv -min-count $mincount
+../word2vecf/word2vecf -train $deps -wvocab $wv -cvocab $cv -output $out -size $dim -negative $ns -threads $p
