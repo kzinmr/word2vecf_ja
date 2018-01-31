33e71d9a9c91844e1c7587caf6df653bc7824220
Kazuki Inamura <kzinmr109@gmail.com>
2018-02-01 00:19:01 +0900

    dumpcv

--- preprocess/word2vecf.zsh
+++ preprocess/word2vecf.zsh
@@ -9,5 +9,8 @@ dir=$deps:h
 cv=${dir}/cv
 wv=${dir}/wv
 out=${dir}/dim${dim}vecs
+outcv=${dir}/dim${dim}context-vecs
 ../word2vecf/count_and_filter -train $deps -cvocab $cv -wvocab $wv -min-count $mincount
-../word2vecf/word2vecf -train $deps -wvocab $wv -cvocab $cv -output $out -size $dim -negative $ns -threads $p
+../word2vecf/word2vecf -train $deps -wvocab $wv -cvocab $cv -output $out -size $dim -negative $ns -threads $p -dumpcv $outcv
+nkf -w $out > ${out}.utf8
+nkf -w $outcv > ${outcv}.utf8
