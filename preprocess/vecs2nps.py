cc4aea95d9f831e05c4a7441dbf8ad9fa10f0a24
Kazuki Inamura <kzinmr109@gmail.com>
2018-02-01 10:41:17 +0900

    fix

--- preprocess/vecs2nps.py
+++ preprocess/vecs2nps.py
@@ -6,16 +6,18 @@ foutname=sys.argv[2]
 with open(filename, mode='rt', encoding='utf8') as fh:
     first = fh.readline()
     size = list(map(int, first.strip().split(' ')))
-    wvecs = np.zeros((size[0], size[1]), float)
+    dim = size[1]
+    wvecs = [] # np.zeros((size[0], size[1]), float)
     vocab = []
-    for i, line in enumerate(fh):
+    for line in fh:
         line = line.strip().split(' ')
-        try:
-            wvecs[i,] = np.array(list(map(float, line[1:])))
+        v = list(map(float, line[1:]))
+        if len(v) == dim:
+            wvecs.append(v)
             vocab.append(line[0]+'\n')
-        except:
+        else:
             print(line[0])
-
+    wvecs = np.array(wvecs)
 np.save(foutname+".npy", wvecs)
 with open(foutname+".vocab", "wt") as outf:
     outf.write("".join(vocab)+"\n")
